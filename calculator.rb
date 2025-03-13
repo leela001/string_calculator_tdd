bb519fd9b671c9d41b7eecec7ad1ecc57a31df45
Leelapavan <leelapavankumarchitirala25@gmail.com>
2025-03-13 08:22:10 +0530

    Implement add method for summing comma-separated numbers
    Added test when the input contains string with comma(,) separation

--- calculator.rb
+++ calculator.rb
@@ -1,2 +1,17 @@
 class Calculator
+    def add(input_str)
+
+        # default delim
+        delimiter = ","
+
+        # splits the input string into an array of substrings
+        str_array = input_str.split(delimiter)
+
+        # converts to an integer array
+        nums = str_array.map(&:to_i)
+
+        sum = 0
+        nums.each {|num| sum += num}
+        return sum
+    end
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -1,4 +1,12 @@
-require 'calculator'
+require 'rspec'
+require_relative 'calculator'
 
 RSpec.describe Calculator do
+    describe 'test add' do
+        it 'test when the input contains string with comma(,) separation' do
+            calc = Calculator.new
+            res = calc.add("5,10,15")
+            expect(res).to eq(30)
+        end
+    end
 end
